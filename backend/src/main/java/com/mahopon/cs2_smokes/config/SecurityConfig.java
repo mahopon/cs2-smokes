fb4f94cb5c47c0c92e971be9623b602f18f33c2e
Chong Yao <56928030+mahopon@users.noreply.github.com>
2025-07-06 04:55:06 +0800

    New:Add config package

--- backend/src/main/java/com/mahopon/cs2_smokes/config/SecurityConfig.java
+++ backend/src/main/java/com/mahopon/cs2_smokes/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.mahopon.cs2_smokes;
+package com.mahopon.cs2_smokes.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
