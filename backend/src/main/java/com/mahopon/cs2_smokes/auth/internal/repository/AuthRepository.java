8ea231fe6a644e4532fa5d43793aaf5b9c53eb2d
Chong Yao <56928030+mahopon@users.noreply.github.com>
2025-07-23 22:40:26 +0800

    Change:change to JPARepository

--- backend/src/main/java/com/mahopon/cs2_smokes/auth/internal/repository/AuthRepository.java
+++ backend/src/main/java/com/mahopon/cs2_smokes/auth/internal/repository/AuthRepository.java
@@ -3,12 +3,12 @@ package com.mahopon.cs2_smokes.auth.internal.repository;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.mahopon.cs2_smokes.auth.api.model.Auth;
 
 @Repository
-public interface AuthRepository extends CrudRepository<Auth, UUID>{
+public interface AuthRepository extends JpaRepository<Auth, UUID> {
     Optional<Auth> findByEmail(String email);
 }
--- backend/src/main/java/com/mahopon/cs2_smokes/user/internal/repository/UserRepository.java
+++ backend/src/main/java/com/mahopon/cs2_smokes/user/internal/repository/UserRepository.java
@@ -2,11 +2,12 @@ package com.mahopon.cs2_smokes.user.internal.repository;
 
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.mahopon.cs2_smokes.user.internal.model.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
